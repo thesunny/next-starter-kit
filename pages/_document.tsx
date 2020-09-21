a2bffa0d031ab79e661d16bf69edf5aec421ac7b
Sunny Hirai <thesunny@gmail.com>
2020-09-21 15:39:55 -0700

    Switch pages to tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,11 +5,6 @@
 import Document, { Head, Html, Main, NextScript } from "next/document"
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
-
   render() {
     return (
       <Html>
