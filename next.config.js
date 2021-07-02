08293abd7ff6ef50e76f5cb4390f98e721fc1e61
Sunny Hirai <thesunny@gmail.com>
2021-07-02 12:38:53 -0700

    Upgraded dependencies but not dev dependencies

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-const withSourceMaps = require("@zeit/next-source-maps")
-const withCSS = require("@zeit/next-css")
+/**
+ * Previously, `next.config.js` was used for adding `@zeit/next-source-maps`
+ * and `@zeit/next-css` but these are not built in.
+ *
+ * For more information about using `next.config.js` see the documentation:
+ * <https://nextjs.org/docs/api-reference/next.config.js/introduction>
+ */
 
-module.exports = withSourceMaps(
-  withCSS({
-    webpack(config, options) {
-      return config
-    },
-  })
-)
+module.exports = {}
